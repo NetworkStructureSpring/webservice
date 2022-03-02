28357234fe2c5c6a8446a921ddedbf5126cc0213
Sonali Singh <singh.sonal@northeastern.edu>
2022-03-01 20:02:22 -0500

    Testing workflow

--- postgress.sh
+++ postgress.sh
@@ -1,28 +1,6 @@
 #! /bin/bash
+sudo ls -al
 
-sudo yum update -yum
-sudo amazon-linux-extras install postgresql9.6
-sudo tee /etc/yum.repos.d/pgdg.repo<<EOF
-[pgdg13]
-name=PostgreSQL 13 for RHEL/CentOS 7 - x86_64
-baseurl=https://download.postgresql.org/pub/repos/yum/13/redhat/rhel-7-x86_64
-enabled=1
-gpgcheck=0
-EOF
-sudo yum install postgresql13 postgresql13-server -y
-sudo /usr/pqsql-13/bin/postgresql-13-setup initdb
-
-sudo systemctl stop postgresql-13.service
-sudo systemctl start postgresql-13.service
-sudo systemctl enable postgresql-13.service
-sudo systemctl status postgresql-13.service
-
-sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD '123Fall@2021';"
-
-
-sudo systemctl stop postgresql-13.service
-sudo systemctl start postgresql-13.service
-sudo systemctl status postgresql-13.service
 
 
 
