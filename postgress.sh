b186d891f20837439c0379be1413158f3d8e3fc6
Sonali Singh <singh.sonal@northeastern.edu>
2022-03-01 21:45:19 -0500

    Testing workflow

--- postgress.sh
+++ postgress.sh
@@ -28,10 +28,10 @@ sudo yum install -y nodejs
 sudo node -v
 sudo unzip webservice.zip -d webservice
 sudo ls -al
-cd webservice
+cd webservice/app
 sudo ls -al
 sudo npm i -g pm2
-sudo pm2 start /app/service.js
+sudo pm2 start /service.js
 sudo pm2 startup systemd
 sudo pm2 save
 sudo pm2 list
