ff79378e6804ff7bc2bb8fb44603cef20f3b6f7b
Sonali Singh <singh.sonal@northeastern.edu>
2022-03-10 02:14:56 -0500

    changes

--- app/Models/Profile.js
+++ app/Models/Profile.js
@@ -1,9 +1,9 @@
 import Sequelize from 'sequelize';
 
-// const sequelize = new Sequelize('UserAccount', 'postgres', '123Fall@2021', {
-//     dialect:  'postgres'
-//   });
-const sequelize = new Sequelize('postgres://postgres:123Fall@2021@localhost:5432/UserAccount')
+const sequelize = new Sequelize('UserAccount', 'postgres', '123Fall@2021', {
+    dialect:  'postgres'
+  });
+// const sequelize = new Sequelize('postgres://postgres:123Fall@2021@localhost:5432/UserAccount')
 sequelize
 .authenticate()
 .then(() => {
