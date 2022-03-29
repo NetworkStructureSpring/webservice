58ed19f41b5e2d27329f9a66adcabbe57746b63f
Sonali Singh <singh.sonal@northeastern.edu>
2022-03-28 21:17:22 -0400

    Changes

--- postgres.sh
+++ postgres.sh
@@ -1,25 +1,26 @@
 #! /bin/bash
-curl --silent --location https://rpm.nodesource.com/setup_17.x | sudo bash -
-sudo yum install -y nodejs
-sudo node -v
-sudo unzip webservice.zip -d webservice
-sudo ls -al
-cd ~/webservice
+# curl --silent --location https://rpm.nodesource.com/setup_17.x | sudo bash -
+# sudo yum install -y nodejs
+# sudo node -v
+# sudo unzip webservice.zip -d webservice
+# sudo ls -al
+# cd ~/webservice
 
-sudo npm install pm2@latest -g
-sudo pm2 startup systemd --service-name myapp
-sudo pm2 start server.js
-sudo pm2 save
+# sudo npm install pm2@latest -g
+# sudo pm2 startup systemd --service-name myapp
+# sudo pm2 start server.js
+# sudo pm2 save
 
-cd /home/ec2-user
+
 sudo yum update
 sudo yum install ruby
 sudo yum install wget
 CODEDEPLOY_BIN="/opt/codedeploy-agent/bin/codedeploy-agent"
 $CODEDEPLOY_BIN stop
 yum erase codedeploy-agent -y
-wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
-chmod +x ./install
+cd /home/ec2-user
+sudo wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+sudo chmod +x ./install
 sudo ./install auto
 sudo service codedeploy-agent start
 sudo service codedeploy-agent status
