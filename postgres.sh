acf204657ad4674537c33fefd1b3258f66e4967c
Sonali Singh <singh.sonal@northeastern.edu>
2022-03-28 21:32:08 -0400

    Changes

--- postgres.sh
+++ postgres.sh
@@ -12,24 +12,20 @@
 # sudo pm2 save
 
 
-# sudo yum update
-# sudo yum install ruby
-# sudo yum install wget
-# #! /bin/bash
-# # CODEDEPLOY_BIN="/opt/codedeploy-agent/bin/codedeploy-agent"
-# # $CODEDEPLOY_BIN stop
-# # yum erase codedeploy-agent -y
-# cd /home/ec2-user
-# sudo wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
-# sudo chmod +x ./install
-# sudo ./install auto
-# sudo service codedeploy-agent start
-# sudo service codedeploy-agent status
 sudo yum update
 sudo yum install ruby
 sudo yum install wget
-wget https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/install
-chmod +x ./install
+#! /bin/bash
+# CODEDEPLOY_BIN="/opt/codedeploy-agent/bin/codedeploy-agent"
+# $CODEDEPLOY_BIN stop
+# yum erase codedeploy-agent -y
+cd /home/ec2-user
+sudo wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+sudo ln -s /usr/local/bin/ruby /usr/bin/ruby
+sudo chmod +x ./install
 sudo ./install auto
+sudo service codedeploy-agent start
+sudo service codedeploy-agent status
+
 
 
