030656f4cdf1a6e94d45380eaa355f0752e27384
Sonali Singh <singh.sonal@northeastern.edu>
2022-03-28 21:47:01 -0400

    Changes

--- postgres.sh
+++ postgres.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
-# curl --silent --location https://rpm.nodesource.com/setup_17.x | sudo bash -
-# sudo yum install -y nodejs
-# sudo node -v
-# sudo unzip webservice.zip -d webservice
-# sudo ls -al
-# cd ~/webservice
+curl --silent --location https://rpm.nodesource.com/setup_17.x | sudo bash -
+sudo yum install -y nodejs
+sudo node -v
+sudo unzip webservice.zip -d webservice
+sudo ls -al
+cd ~/webservice
 
-# sudo npm install pm2@latest -g
-# sudo pm2 startup systemd --service-name myapp
-# sudo pm2 start server.js
-# sudo pm2 save
+sudo npm install pm2@latest -g
+sudo pm2 startup systemd --service-name myapp
+sudo pm2 start server.js
+sudo pm2 save
 
 
 sudo yum update
