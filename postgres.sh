d3801e113fcdab87fd0edab7c564a61a32b06990
Sonali Singh <singh.sonal@northeastern.edu>
2022-03-28 21:40:42 -0400

    Changes

--- postgres.sh
+++ postgres.sh
@@ -14,6 +14,7 @@
 
 sudo yum update
 sudo yum install wget ruby -y
+cd /home/ec2-user
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
