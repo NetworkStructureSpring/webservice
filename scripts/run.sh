e52fac6fd9c95e03ac6072139cefcff6e4e9e329
Sonali Singh <singh.sonal@northeastern.edu>
2022-03-31 15:12:14 -0400

    DEmo Related Changes

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/webapp
-pm2 kill
-pm2 start server.js
-pm2 save
+sudo pm2 kill
+sudo pm2 start server.js
+sudo pm2 save
+sudo pm2 startup systemd --service-name myapp
