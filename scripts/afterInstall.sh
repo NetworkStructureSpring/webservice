449486a4d4e12a710a660bf677cbe8442264be35
Sonali Singh <singh.sonal@northeastern.edu>
2022-04-03 04:01:34 -0400

    Cloud watch added

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -4,4 +4,4 @@ sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-c
 
 sudo chmod +rwx /opt/aws/amazon-cloudwatch-agent/doc/amazon-cloudwatch-agent-schema.json
 
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ec2-user/amazon-cloudwatch-agent-schema.json  -s
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ec2-user/webservice/amazon-cloudwatch-agent-schema.json  -s
