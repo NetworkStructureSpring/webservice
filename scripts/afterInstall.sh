c9336d3d6758d64ad5e8a52fa5c55d1e06221339
Sonali Singh <singh.sonal@northeastern.edu>
2022-04-03 03:54:11 -0400

    Cloud watch added

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /home/ec2-user
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:./Config.json
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file: ../Config.json
