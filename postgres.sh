c864f1a1838a03fb2bdaae610b8ea680e19cbd7b
Sonali Singh <singh.sonal@northeastern.edu>
2022-04-02 23:32:11 -0400

    Cloud watch added

--- postgres.sh
+++ postgres.sh
@@ -18,8 +18,7 @@ chmod +x ./install
 sudo ./install auto
 sudo service codedeploy-agent start
 sudo service codedeploy-agent status
-sudo wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
-sudo dpkg -i -E ./amazon-cloudwatch-agent.deb
+sudo yum install amazon-cloudwatch-agent
 #sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:./Config.json
 
 
