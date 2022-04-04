402cc0d35577cd29e3f7a3748f675652ffc5af5a
Sonali Singh <singh.sonal@northeastern.edu>
2022-04-04 02:01:53 -0400

    Latesty

--- app/controllers/Health.js
+++ app/controllers/Health.js
@@ -1,5 +1,4 @@
 import * as healthService from '../services/Health.js';
-import lawgs from 'index';
 
 //This method handles success status code
 const setSuccessResponse = (data, response) => {
@@ -11,13 +10,7 @@ const errorHandler = (data, response) => {
 }
 export const getServiceHealth = async (request, response) => {
     try {
-        lawgs.config({
-            aws: {
-                region: 'us-east-1' /* Required */
-            }
-        });
-        var logger  = lawgs.getOrCreate('amazon-cloudwatch-agent.log'); /* LogGroup */
-        logger.log('touchdown', { team: 'Patriots', weight: 7 });
+        
         const result = await healthService.getServiceHealth(); 
         setSuccessResponse(result, response);
     }
