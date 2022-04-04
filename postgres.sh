7721dc0ff811020245b34d20cbb607fe995d7c32
Sonali Singh <singh.sonal@northeastern.edu>
2022-04-04 00:42:38 -0400

    Test

--- postgres.sh
+++ postgres.sh
@@ -23,7 +23,7 @@ sudo ./install auto
 sudo service codedeploy-agent start
 sudo service codedeploy-agent status
 sudo yum install amazon-cloudwatch-agent -y
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:./Config.json
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -s -c file:./amazon-cloudwatch-agent-schema.json
 
 
 
