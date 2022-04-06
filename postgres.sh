da7acba6ab2acd634c703a200783a1e80b1cdc6e
Sonali Singh <singh.sonal@northeastern.edu>
2022-04-05 23:04:05 -0400

    Changes

--- postgres.sh
+++ postgres.sh
@@ -7,7 +7,8 @@ sudo ls -al
 cd ~/webservice
 sudo npm install pm2@latest -g
 sudo pm2 kill
-sudo pm2 startOrReload Config.js 
+#sudo pm2 start server.js
+sudo pm2 start Config.js 
 sudo pm2 save
 sudo pm2 startup systemd --service-name myapp
 
