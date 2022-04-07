71abbd62b228d80397810a55ab41c416ee321a01
Sonali Singh <singh.sonal@northeastern.edu>
2022-04-07 00:14:27 -0400

    Changes

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,6 +3,6 @@ cd /home/ec2-user/webservice
 sudo pm2 kill
 sudo npm i
 # sudo pm2 start server.js
-sudo pm2 start server.js
+sudo pm2 start ecosystem.config.json
 sudo pm2 save
 sudo pm2 startup systemd --service-name myapp
