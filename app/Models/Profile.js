2eb5b72016b76ae6b78aaca4b11504d96a85efe0
Sonali Singh <singh.sonal@northeastern.edu>
2022-04-21 11:18:47 -0400

    Changes

--- app/Models/Profile.js
+++ app/Models/Profile.js
@@ -1,7 +1,6 @@
 import Sequelize from 'sequelize';
-// import fs from 'fs';
-// console.log(pwd);
-// const rdsCa = fs.readFileSync(pwd + '/us-east-1-bundle.pem');
+import fs from 'fs';
+const rdsCa = fs.readFileSync(process.env.PWD + '/us-east-1-bundle.pem');
 var connectionString = "";
 //const sequelize = new Sequelize('postgres://postgres:123Fall@2021@localhost:5432/UserAccount')
 function setConnectionString()
@@ -18,11 +17,10 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, pr
         max: 10,
         idle: 30000
     },
-    // },
-    // ssl: {
-    //     rejectUnauthorized: true,
-    //     ca: [rdsCa]
-    // }
+    ssl: {
+        rejectUnauthorized: true,
+        ca: [rdsCa]
+    }
 })
 async function connect() {
     console.log('Checking database connection...');
