74c02dc430d246c40b7cc18b52b932a9fde12b16
Sonali Singh <singh.sonal@northeastern.edu>
2022-04-21 12:24:24 -0400

    Changes

--- postgres.sh
+++ postgres.sh
@@ -18,6 +18,8 @@ CODEDEPLOY_BIN="/opt/codedeploy-agent/bin/codedeploy-agent"
 $CODEDEPLOY_BIN stop
 yum erase codedeploy-agent -y
 cd /home/ec2-user
+sudo yum install -y postgresql13 -y
+sudo psql --version
 wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
 chmod +x ./install
 sudo ./install auto
